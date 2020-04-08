e5db460978e9b40c908cd7e6846a0fe598a3c278
Sobira <sobira204@gmail.com>
2020-04-08 22:31:14 +0500

    add method Push

--- ArrayHelperClass.cs
+++ ArrayHelperClass.cs
@@ -48,6 +48,51 @@ namespace HomeWork8
                 arr = newArr;
                 return value;
             }
+            static int Push(ref int[] arr, int x){
+                int[] newArr = new int [arr.Length + 1];
+                for (int i = 0; i < arr.Length; i ++){
+                    newArr[i] = arr[i];
+                }
+                newArr[newArr.Length-1] = x;
+                arr = newArr;
+                return arr.Length;
+            }
+            static int Push(ref string[] arr, string x){
+                string[] newArr = new string[arr.Length + 1];
+                for (int i = 0; i < arr.Length; i ++){
+                    newArr[i] = arr[i];
+                }
+                newArr[newArr.Length-1] = x;
+                arr = newArr;
+                return arr.Length;
+            }
+            static int Push(ref double[] arr, double x){
+                double[] newArr = new double [arr.Length + 1];
+                for (int i = 0; i < arr.Length; i ++){
+                    newArr[i] = arr[i];
+                }
+                newArr[newArr.Length-1] = x;
+                arr = newArr;
+                return arr.Length;
+            }
+            static int Push(ref decimal[] arr, decimal x){
+                decimal[] newArr = new decimal [arr.Length + 1];
+                for (int i = 0; i < arr.Length; i ++){
+                    newArr[i] = arr[i];
+                }
+                newArr[newArr.Length-1] = x;
+                arr = newArr;
+                return arr.Length;
+            }
+            static int Push(ref float[] arr, float x){
+                float[] newArr = new float [arr.Length + 1];
+                for (int i = 0; i < arr.Length; i ++){
+                    newArr[i] = arr[i];
+                }
+                newArr[newArr.Length-1] = x;
+                arr = newArr;
+                return arr.Length;
+            }
             
         }
     }
